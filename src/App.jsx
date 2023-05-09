183c23f28cd305dee93050f0f2fbd4c8183b1eca
Francisco Bessa <edenilson.victoralves@outlook.com>
2023-05-08 21:01:44 -0300

    added sidebar to choice the food category

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import Navbar from "./components/Navbar"
 import { RestaurantContext } from "./components/context/RestaurantContext"
+import { useState } from "react"
+import restaurantData from "../restaurant-data.json"
+import _ from "lodash"
 
 function App() {
+  const [categories, setCategories] = useState([])
+
+  const updateCategories = () => {
+    setCategories(restaurantData.categories)
+  }
 
   const state = {
+    categories,
+  }
 
+  if (categories && _.isEmpty(categories)) {
+    updateCategories()
   }
 
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
+import { useContext } from 'react'
 import Logo from '../assets/logo.jpg'
+import { RestaurantContext } from './context/RestaurantContext'
 
 const Navbar = () => {
+    const { categories } = useContext(RestaurantContext)
+
     return (
         <div className='navbar'>
-            <img src={Logo} alt="restaurant-logo" className='navbar-restaurant-logo'/>
+            <div className='navbar-logo-title-container'>
+                <img src={Logo} alt="restaurant-logo" className='navbar-restaurant-logo' />
+                <h1 className='navbar-title'>Palate Pleasers: Indulge in Culinary Delights at PlatePal</h1>
+            </div>
+
+            <div className='navbar-categories-container'>
+                {categories.map((categorie) => (
+                    <div key={categorie.id} className='navbar-categorie-content'>
+                        <h2 className="navbar-categorie">{categorie.name}</h2>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
